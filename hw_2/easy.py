7dc93042a0010f0cf89da9705a3d14b12c7776ec
Cabpacob <Cabpacob@github.com>
2022-02-16 22:02:31 +0300

    Implemented hw_2.easy

---
+++ hw_2/easy.py
@@ -0,0 +1,43 @@
+def get_tex_row(row):
+    return ' & '.join(map(str, row))
+
+
+def get_tex_table(table):
+    return '''
+    \\begin{{center}}
+        \\begin{{tabular}}{{{cs}}}
+            \\hline
+            {rows} \\\\
+            \\hline
+        \\end{{tabular}}
+    \\end{{center}}
+    '''.format(
+        cs = ' ' + len(table[0]) * '|c' + '| ',
+        rows = ' \\\\ \n\t\t\t\\hline \n\t\t\t'.join(map(get_tex_row, table))
+        )
+
+
+def get_document_tex(content):
+    return '''\\documentclass{{letter}}
+
+\\begin{{document}}
+{content}
+\\end{{document}}
+'''.format(content=content)
+
+
+table = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+if __name__ == '__main__':
+    rows = len(table)
+    columns = len(table[0])
+
+    def equal_len(row):
+        return len(row) == columns
+
+    assert all(map(equal_len, table)), f'wrong table with {columns} columns and {rows} rows'
+
+    document_tex = get_document_tex(get_tex_table(table))
+    with open('artifacts/easy.tex', 'w') as f:
+        f.write(document_tex)
